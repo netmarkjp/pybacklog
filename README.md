54c1967b4ad39cfb62f3dd266b8e37a41abfcc70
Toshiaki Baba <toshiaki@netmark.jp>
2017-06-22 18:14:11 +0900

    - add utility methods

--- README.md
+++ README.md
@@ -22,6 +22,50 @@ pip install pybacklog
 from pybacklog import BacklogClient
 
 client = BacklogClient("your_space_name", "your_api_key")
+
+# space
+space = client.do("GET", "space")  # GET /api/v2/space
+print(space.get(u"spaceKey"))
+
+# project
+projects = client.projects()
+
+# activity
+activities = client.project_activities("YOUR_PROJECT", {"activityTypeId[]": [1, 2]})
+
+# list issue
+project_id = client.get_project_id("YOUR_PROJECT")
+issues = client.issues({"projectId[]":[project_id], "sort": "dueDate"})
+
+# specified issue
+issue = client.issue("YOUR_PROJECT-999")
+
+# create issue
+project_id = client.get_project_id(project_key)
+issue_type_id = client.issue_types(project_key)[0][u"id"]
+priority_id = client.priorities()[0][u"id"]
+
+client.create_issue(project_id,
+                    u"some summary",
+                    issue_type_id,
+                    priority_id,
+                    {"description": u"a is b and c or d."})
+
+# add comment
+client.add_comment("YOUR_PROJECT-999", u"or ... else e.")
+```
+
+extra parameters are here
+=> [Backlog API Overview \| Backlog Developer API \| Nulab](https://developer.nulab-inc.com/docs/backlog/)
+
+# Unsupported operations ?
+
+Use `do` or let's write code and Pull Request.
+
+```python
+from pybacklog import BacklogClient
+
+client = BacklogClient("your_space_name", "your_api_key")
 space = client.do("GET", "space")  # GET /api/v2/space
 projects = client.do("GET", "projects",
           query_params={"archived": false}
@@ -30,9 +74,9 @@ activities = client.do("GET", "projects/{project_id_or_key}/activities",
           url_params={"project_id_or_key": "myproj"},
           query_params={"activityTypeId[]": [1, 2]}
           )  # GET /api/v2/projects/myproj/activities?activityTypeIds%5B%5D=1&activityTypeIds%5B%5D=2
-
-print(space.get(u"spaceKey"))
 ```
+
+see also [Backlog API Overview \| Backlog Developer API \| Nulab](https://developer.nulab-inc.com/docs/backlog/)
 
 # Development
 
--- pybacklog/__init__.py
+++ pybacklog/__init__.py
@@ -5,7 +5,6 @@ import re
 
 
 class BacklogClient(object):
-    endpoint = ""
 
     def __init__(self, space_name, api_key):
         self.space_name = space_name
@@ -58,5 +57,126 @@ class BacklogClient(object):
         # remove 4 byte characters
         pattern = re.compile(u"[^\u0000-\uD7FF\uE000-\uFFFF]", re.UNICODE)
         for key in request_params.keys():
-            request_params[key] = pattern.sub(u"\uFFFD", request_params[key])
+            if isinstance(request_params[key], unicode):
+                request_params[key] = pattern.sub(u"\uFFFD", request_params[key])
         return request_params
+
+    # -------------------------------
+    # major operations (PR welcome)
+    # -------------------------------
+    #
+    # - required values => args
+    # - optional values => extra_query_params, extra_request_params
+    #     - url_params may always required
+
+    def space(self):
+        """
+        client = BacklogClient("your_space_name", "your_api_key")
+        client.space()
+        """
+        return self.do("GET", "space")
+
+    def projects(self, extra_query_params={}):
+        """
+        client = BacklogClient("your_space_name", "your_api_key")
+        client.projects()
+        client.projects({"archived": "false",})
+        """
+        return self.do("GET", "projects", query_params=extra_query_params)
+
+    def project_activities(self, project_id_or_key, extra_query_params={}):
+        """
+        client = BacklogClient("your_space_name", "your_api_key")
+        client.project_activities("YOUR_PROJECT")
+        client.project_activities("YOUR_PROJECT", {"activityTypeId[]": [1, 2],})
+        """
+        return self.do("get", "projects/{project_id_or_key}/activities",
+                       url_params={"project_id_or_key": project_id_or_key},
+                       query_params=extra_query_params,
+                       )
+
+    def issues(self, extra_query_params={}):
+        """
+        client = BacklogClient("your_space_name", "your_api_key")
+        client.issues()
+
+        project_id = client.get_project_id("YOUR_PROJECT")
+        client.issues({"projectId[]":[project_id], "sort": "dueDate"})
+        """
+        return self.do("GET", "issues", query_params=extra_query_params)
+
+    def issue(self, issue_id_or_key):
+        """
+        client = BacklogClient("your_space_name", "your_api_key")
+        client.issue("YOUR_PROJECT-999")
+        """
+        return self.do("GET", "issues/{issue_id_or_key}",
+                       url_params={"issue_id_or_key": issue_id_or_key},
+                       )
+
+    def issue_types(self, project_id_or_key):
+        """
+        client = BacklogClient("your_space_name", "your_api_key")
+        client.issue_types("YOUR_PROJECT")
+        """
+        return self.do("GET", "projects/{project_id_or_key}/issueTypes",
+                       url_params={"project_id_or_key": project_id_or_key},
+                       )
+
+    def priorities(self):
+        """
+        client = BacklogClient("your_space_name", "your_api_key")
+        client.priorities()
+        """
+        return self.do("GET", "priorities")
+
+    def create_issue(self, project_id, summary, issue_type_id, priority_id, extra_request_params={}):
+        """
+        client = BacklogClient("your_space_name", "your_api_key")
+        project_key = "YOUR_PROJECT"
+
+        project_id = client.get_project_id(project_key)
+        issue_type_id = client.issue_types(project_key)[0][u"id"]
+        priority_id = client.priorities()[0][u"id"]
+
+        client.create_issue(project_id,
+                            u"some summary",
+                            issue_type_id,
+                            priority_id,
+                            {"description": u"a is b and c or d."})
+        """
+        request_params = extra_request_params
+        request_params["projectId"] = project_id
+        request_params["summary"] = summary
+        request_params["issueTypeId"] = issue_type_id
+        request_params["priorityId"] = priority_id
+
+        return self.do("POST", "issues",
+                       request_params=request_params,
+                       )
+
+    def add_comment(self, issue_id_or_key, content, extra_request_params={}):
+        """
+        client = BacklogClient("your_space_name", "your_api_key")
+        client.add_comment("YOUR_PROJECT-999", u"or ... else e.")
+        """
+        request_params = extra_request_params
+        request_params["content"] = content
+        return self.do("POST", "issues/{issue_id_or_key}/comments",
+                       url_params={"issue_id_or_key": issue_id_or_key},
+                       request_params=request_params,
+                       )
+
+    # -------------------------------
+    # extra utilities (PR welcome)
+    # -------------------------------
+
+    def get_project_id(self, project_key_or_name):
+        projects = self.projects()
+        for p in projects:
+            if p[u"projectKey"] == project_key_or_name:
+                return p[u"id"]
+        for p in projects:
+            if p[u"name"] == project_key_or_name:
+                return p[u"id"]
+        return None
