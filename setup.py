762ef4eff2602fa5bf24fc18a93efd8fde11c81f
Toshiaki Baba <toshiaki@netmark.jp>
2018-11-12 11:36:23 +0900

    version up

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pybacklog",
-    version="0.1.5",
+    version="0.1.6",
     description="Backlog API v2 Client",
     author="Toshiaki Baba",
     author_email="toshiaki@netmark.jp",
